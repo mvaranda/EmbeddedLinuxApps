ec83edc430073dce99030c86abb234e2a665064c
Marcelo Varanda <m@varanda.ca>
2021-11-19 12:59:34 -0500

    add helper script.

---
+++ LinuxBuild/rpi-build/img_to_home.sh
@@ -0,0 +1,5 @@
+echo on
+echo "***************** copying image to home directory ************"
+cp -v tmp/deploy/images/raspberrypi3/core-image-base-raspberrypi3.rpi-sdimg ~/rpi-varanda.img
+echo "**** image copied to $HOME/rpi-varanda.img ****"
+
