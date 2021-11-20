4641ef365e221f89f847988675678fb3a262400e
Varanda <m@varanda.ca>
2021-11-20 09:36:04 -0500

    add helper script.

---
+++ LinuxBuild/rpi-build/img_sato_to_home.sh
@@ -0,0 +1,5 @@
+echo on
+echo "***************** copying image to home directory ************"
+cp -v tmp/deploy/images/raspberrypi3/core-image-sato-raspberrypi3.rpi-sdimg ~/rpi-sato.img
+echo "**** image copied to $HOME/rpi-sato.img ****"
+
--- LinuxBuild/rpi-build/img_to_home.sh
+++ LinuxBuild/rpi-build/img_to_home.sh
@@ -1,5 +1,5 @@
 echo on
 echo "***************** copying image to home directory ************"
-cp -v tmp/deploy/images/raspberrypi3/core-image-base-raspberrypi3.rpi-sdimg ~/rpi-varanda.img
-echo "**** image copied to $HOME/rpi-varanda.img ****"
+cp -v tmp/deploy/images/raspberrypi3/core-image-base-raspberrypi3.rpi-sdimg ~/rpi-base.img
+echo "**** image copied to $HOME/rpi-base.img ****"
 
